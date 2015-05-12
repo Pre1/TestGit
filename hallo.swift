41cdc6812cd761b961f44c90b45aef5d5b298a4a
Abdullah Abnmi <anabanmi@hotmail.com>
2015-05-12 19:53:03 +0300

    info about git

--- hallo.swift
+++ hallo.swift
@@ -1,7 +1,17 @@
 #!/usr/bin/env xcrun swift
 
 println("Hey, Git!")
+
 // make changes
 // add the changes -> git add .
 // commit changes to repo w/ msg
 // -> git commit -m "[Text]"
+
+// commit message best practices
+
+// - short single-line summary (less than 50 characters)
+
+// - optionally followed by a blank line and a more complete
+// description
+
+// - keep each line to less than 72 characters
