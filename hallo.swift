7ba8de818d864af3f07a2958919e1ab519d4159b
Abdullah Abanmi <anabanmi@hotmail.com>
2015-12-30 06:32:05 +0300

    commit hallow swift

---
+++ hallo.swift
@@ -0,0 +1,19 @@
+#!/usr/bin/env xcrun swift
+
+println("Hey, Git!")
+
+// make changes
+// add the changes -> git add .
+// commit changes to repo w/ msg
+// -> git commit -m "[Text]"
+
+// commit message best practices
+
+// - short single-line summary (less than 50 characters)
+
+// - optionally followed by a blank line and a more complete
+// description
+
+// - keep each line to less than 72 characters
+
+// HEAD is pointer that points to last commit in current repo
